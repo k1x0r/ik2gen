aa5d325700f9758b069e91f0eb3f812f69d885a2
k1x <3296260+k1x0r@users.noreply.github.com>
2022-11-28 11:58:33 +0200

    Changed /usr/local/bin/ to /opt/local/bin/ for m1 macs

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash 
 set -v -x #echo on
 
+git pull
 unset K2PROJ
 swift package generate-xcodeproj
 export K2PROJ=true
 swift build -c release
 
 echo "Removing old ik2gen..."
-rm /usr/local/bin/ik2gen
+rm /opt/local/bin/ik2gen
 chmod 755 ik2gen
-cp ik2gen /usr/local/bin/
+cp ik2gen /opt/local/bin/
 echo "Installation directory: $PWD"
-sed -i '' "s|#_INSTALL_DIR_#|$PWD/|g" "/usr/local/bin/ik2gen"
+sed -i '' "s|#_INSTALL_DIR_#|$PWD/|g" "/opt/local/bin/ik2gen"
 
 
 echo Done
